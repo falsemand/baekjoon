7069205c657120df3e78a10e6556a9e9d7140b91
falseman <falseman_go@naver.com>
2025-01-01 23:52:56 +0900

    cmake backjoon project

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solution()
+{
+    cout<<"Hello C++!"<<endl;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    solution();
+
+    return 0;
+}
